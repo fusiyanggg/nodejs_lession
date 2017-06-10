3eb8a64e13d13ee309b5d274844289d865bff08d
fusiyang <905867333@qq.com>
2017-06-11 01:34:54 +0800

    node中线程和进程的理解

--- day1/2.node/2.process.js
+++ day1/2.node/2.process.js
@@ -2,11 +2,11 @@
 //1. 区分本地开发环境 和线上开发环境
 //设置环境变量 set NODE_ENV=developement 设置环境变量
 console.log(process.env);
-let url =''
-if(process.env.NODE_ENV === "hello"){
-    url =  'http://localhost:8080';
+let url = '';
+if (process.env.NODE_ENV === "hello") {
+    url = 'http://localhost:8080'; //->在webstorm里面运行js代码
     console.log('开发环境 ');
-}else{
-    url = 'http://www.baidu.com';
+} else {
+    url = 'http://www.baidu.com';  //->在外部node环境下运行js代码
     console.log('线上环境');
 }
