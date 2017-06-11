b28d5e94fca2277aba577adb7e3531233388a2ee
fusiyang <905867333@qq.com>
2017-06-11 17:11:34 +0800

    发布订阅模式

--- day2/util/2.on-emi.js
+++ day2/util/2.on-emi.js
@@ -1,12 +1,35 @@
 function Girl() {
-    this._events = {}
+    this._events = {} //给实例化的对象一个私有的事件池
 }
 
 let girl = new Girl();
 
+//在类的原型上定义订阅方法，使得所有的实例都能继承到订阅(绑定)
 Girl.prototype.on = function (eventName, callback) {
+//进来先判断事件池中有没有存在传入的事件
+    if (this._events[eventName]) { //因为event是一个变量，所以不能用.访问，必须用[]
+//如果存在这个事件名，则在这个事件队列里面添加一个新的执行方法
+        this._events[eventName].push(callback)
+        //->>
+        // _event = {
+        //     eventName: [fn1, callback],
+        //     eventName1: [fn12, fn2],
+        // }
 
+    } else {//如果没有这个事件，则将这个事件名添加到事件池中，并且绑定的方法存入数组中
+        this._events[eventName] = [callback]
+    }
 };
+
+//根据发布函数的参数(事件名) 执行对应事件中的方法
+Girl.prototype.emit = function (eventName) {
+    if (this._events[eventName]) {
+        this._events[eventName].forEach(function (item) {
+            item()
+        })
+    }
+};
+
 
 function cry() {
     console.log('cry');
@@ -18,4 +41,4 @@ function die() {
 
 girl.on('run', cry);
 girl.on('run', die);
-girl.emit('run', 'par');
+girl.emit('run');
